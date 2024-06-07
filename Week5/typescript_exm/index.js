7708f288bebdf4a4f438fdc8bde2822b4e203cba
Emmanuel Onu <emmanuel.onu@robogarden.ca>
2024-06-07 17:23:15 -0600

    Added codes on typescript programming

--- Week5/typescript_exm/index.js
+++ Week5/typescript_exm/index.js
@@ -1,4 +1,62 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var x = 4;
 var y = 10;
 console.log(x + y);
 console.log('This is programming');
+var a = 3;
+a = true;
+a = "dddd";
+var arr = [23, true, "eeee"];
+var names = ["Haben", "Peter", "Kushan", "Jay"];
+var numbers = [23, 11, 55, 66, 33];
+//Index
+console.log(numbers[2]); //55
+var person = { name: "John Wick", age: 23, country: "US" };
+//Key
+console.log(person['age']); //23
+console.log(person.age); //23
+var car1 = { model: 'Nissan', doors: 3, isElectric: false };
+var Car = /** @class */ (function () {
+    //Constructor method
+    function Car(model, doors, isElectric) {
+        this.model = model;
+        this.doors = doors;
+        this.isElectric = isElectric;
+    }
+    //other methods
+    Car.prototype.displayMake = function () {
+        console.log('The car make is ' + this.model);
+    };
+    return Car;
+}());
+var car2 = new Car("Mazda", 4, true); //creating a new instance of class Car
+var car3 = new Car("Toyota", 6, false);
+car3.displayMake();
+car2.displayMake();
+//SportsCar is the child class and Car is the parent class.
+var SportsCar = /** @class */ (function (_super) {
+    __extends(SportsCar, _super);
+    function SportsCar(model, doors, isElectric, wheelDiameter) {
+        var _this = _super.call(this, model, doors, isElectric) || this;
+        _this.wheelDiameter = wheelDiameter;
+        return _this;
+    }
+    return SportsCar;
+}(Car));
+var car4 = new SportsCar("Ferrari", 2, false, 21);
+car4.displayMake();
+console.log(car4.wheelDiameter);
--- Week5/typescript_exm/index.ts
+++ Week5/typescript_exm/index.ts
@@ -1,4 +1,69 @@
-let x = 4;
-let y = 10;
+let x:number = 4;
+let y:number = 10;
 console.log(x + y);
 console.log('This is programming')
+
+let a: any = 3;
+a = true;
+a = "dddd"
+
+let arr = [23, true, "eeee"];
+let names: string[] = ["Haben", "Peter", "Kushan", "Jay"];
+let numbers: Array<number> = [23, 11, 55, 66, 33];
+//Index
+console.log(numbers[2]); //55
+
+
+let person = {name: "John Wick", age: 23, country: "US"};
+//Key
+console.log(person['age']); //23
+console.log(person.age); //23
+
+let car1: ICar = {model: 'Nissan', doors: 3, isElectric: false};
+
+class Car implements ICar{
+    //Properties
+    model: string;
+    doors: number;
+    isElectric: boolean;
+
+    //Constructor method
+    constructor(model: string, doors: number, isElectric: boolean){
+        this.model = model;
+        this.doors = doors;
+        this.isElectric = isElectric;
+    }
+
+    //other methods
+    displayMake(): void{
+        console.log('The car make is ' + this.model);
+    }
+}
+
+let car2 = new Car("Mazda", 4, true); //creating a new instance of class Car
+let car3 = new Car("Toyota", 6, false);
+
+car3.displayMake();
+car2.displayMake();
+
+//SportsCar is the child class and Car is the parent class.
+class SportsCar extends Car{
+    wheelDiameter: number;
+
+    constructor(model: string, doors: number, isElectric: boolean, wheelDiameter: number){
+        super(model, doors, isElectric); //super method refers to the parent Class constructor.
+
+        this.wheelDiameter = wheelDiameter;
+    }
+}
+
+let car4 = new SportsCar("Ferrari", 2, false, 21);
+car4.displayMake();
+console.log(car4.wheelDiameter);
+
+
+interface ICar {
+    model: string;
+    doors: number;
+    isElectric: boolean;
+}
