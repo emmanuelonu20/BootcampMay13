2c914f04b1513923438c31f6bc3d1da8c9e7cec0
Emmanuel Onu <emmanuel.onu@robogarden.ca>
2024-06-17 17:38:08 -0600

    Added a new angular project for forms

---
+++ Week6/formsapp/src/app/components/studentform/studentform.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-studentform',
+  templateUrl: './studentform.component.html',
+  styleUrl: './studentform.component.css'
+})
+export class StudentformComponent {
+
+  //setup form group and form controls
+
+  studentForm: FormGroup;
+
+  constructor(private fb: FormBuilder){
+    this.studentForm = fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      country: ['']
+    });
+  }
+
+  onSubmit(){
+    console.log(this.studentForm.value);
+  }
+
+  //Getters
+
+  //Return the name form control
+  get nameFormControl(){
+    return this.studentForm.get('name')!;
+  }
+
+  //Return the email form control
+  get emailFormControl(){
+    return this.studentForm.get('email')!;
+  }
+
+  //Return the country form control
+  get countryFormControl(){
+    return this.studentForm.get('country')!;
+  }
+
+}
